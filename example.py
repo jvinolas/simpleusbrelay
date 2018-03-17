543abe9d5cdda5946da42e8259a565fb6eab5b81
root <root@pepino.localdomain>
2018-03-17 09:08:03 +0100

    Fixed typos on vars

--- example.py
+++ example.py
@@ -3,8 +3,11 @@ import time
 import simpleusbrelay
 
 # initialize the library with the idVendor and Product id
-relaycontroller=simpleusbrelay(idVendor=16c0, idProduct=05df)
+relaycontroller=simpleusbrelay.simpleusbrelay(Vendor=0x16c0, Product=0x05df)
 #turn on relay 1
-relaycontroller.array_on(1)
+relaycontroller.array_on(2)
+relaycontroller=None
+relaycontroller=simpleusbrelay.simpleusbrelay(Vendor=0x16c0, Product=0x05df)
+time.sleep(1)
 #turn of relay 1
-relaycontroller.array_off(1)
+relaycontroller.array_off(2)
